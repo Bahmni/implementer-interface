12db0ae4e0f8f547235b85dfa4fd1dbeb4461e6c
bitweft <bitweft@gmail.com>
2016-07-29 07:52:58 +0530

    Shashi, Lavanya | #1814 | Add support for POST method

--- src/common/utils/httpInterceptor.js
+++ src/common/utils/httpInterceptor.js
@@ -14,4 +14,23 @@ export const httpInterceptor = {
         error.response = response;
         throw error;
       }),
+
+  post: (url, jsonBody) =>
+    fetch(url, {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(jsonBody),
+    })
+      .then((response) => {
+        if (response.status >= 200 && response.status < 300) {
+          return response.json();
+        }
+        const error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+      }),
 };
--- test/common/utils/httpInterceptor.spec.js
+++ test/common/utils/httpInterceptor.spec.js
@@ -7,7 +7,7 @@ chai.use(chaiEnzyme());
 
 describe('httpInterceptor', () => {
   describe('get', () => {
-    beforeEach(() => {
+    afterEach(() => {
       fetchMock.restore();
     });
 
@@ -24,6 +24,35 @@ describe('httpInterceptor', () => {
     it('should throw an error when status is not 2xx', (done) => {
       fetchMock.mock('/someUrl', 404);
       httpInterceptor.get('/someUrl')
+        .then(() => {})
+        .catch(err => {
+          expect(err.response.status).to.eql(404);
+          done();
+        });
+    });
+  });
+
+  describe('post', () => {
+    afterEach(() => {
+      fetchMock.restore();
+    });
+
+    it('should be called with correct params and should return response ' +
+      'when status is 200', (done) => {
+      fetchMock.post('/someUrl', { data: { id: 123 } });
+      const inputBody = { name: 'someFormName' };
+      httpInterceptor.post('/someUrl', inputBody)
+        .then(res => {
+          expect(fetchMock.calls().matched.length).to.eql(1);
+          expect(res.data.id).to.eql(123);
+          expect(JSON.parse(fetchMock.lastOptions('/someUrl').body)).to.deep.equal(inputBody);
+          done();
+        });
+    });
+
+    it('should throw an error when status is not 200', (done) => {
+      fetchMock.post('/someUrl', 404);
+      httpInterceptor.post('/someUrl', { name: 'someFormName' })
         .then(() => {})
         .catch(err => {
           expect(err.response.status).to.eql(404);
