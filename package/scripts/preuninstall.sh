39ea8f57016aa641fc7cd3a5a78333075db67930
shashikanthgadgay <shashikanthgadgay@gmail.com>
2016-09-02 16:20:36 +0530

    Shashi, Jaswanth  | Removing symlink only on uninstall but not on upgrade

--- package/scripts/preuninstall.sh
+++ package/scripts/preuninstall.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-rm -rf /var/www/implementer_interface
+if [ $1 -eq 0 ]; then
+    rm -rf /var/www/implementer_interface
+    rm -rf /etc/httpd/conf.d/implementer_interface_ssl.conf
+fi
