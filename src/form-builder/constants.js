cab209e33ccd512099753e29e298c4382811e176
bitweft <bitweft@gmail.com>
2016-11-02 17:53:06 +0530

    Hanisha, Lavanya | #2256 | Get allowDecimal detail from concept

--- src/form-builder/constants.js
+++ src/form-builder/constants.js
@@ -3,7 +3,7 @@ export const formBuilderConstants = {
   formUrl: '/openmrs/ws/rest/v1/form',
   formResourceUrl: (formUuid) => `/openmrs/ws/rest/v1/form/${formUuid}/resource`,
   conceptUrl: '/openmrs/ws/rest/v1/concept',
-  conceptRepresentation: 'custom:(uuid,display,name:(uuid,name),' +
+  conceptRepresentation: 'custom:(uuid,display,allowDecimal,name:(uuid,name),' +
   'conceptClass:(uuid,name),datatype:(uuid,name),setMembers)',
   formResourceDataType: 'org.openmrs.customdatatype.datatype.FreeTextDatatype',
 };
