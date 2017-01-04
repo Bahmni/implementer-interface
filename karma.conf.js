39e2513bc38e06c0fea00c607193ab1f38b5ca4f
Bijayini <bijayinp@thoughtworks.com>
2017-01-04 10:50:04 +0530

    Bijayini | Karma config port change

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     files: [
       'test/index.js'
     ],
-    port: 8006,
+    port: 8080,
     captureTimeout: 60000,
     browserNoActivityTimeout: 60000,
     frameworks: ['mocha', 'chai'],
