43c6c07404ad958a7d7dc29d734e4068d236cf98
Krishna Kumar Ravi <kravi@thoughtworks.com>
2017-01-17 19:10:44 +0530

    Krishna | #0000 | Fixed the Scroll in the Left Panel

--- src/form-builder/components/FormDetail.jsx
+++ src/form-builder/components/FormDetail.jsx
@@ -1,85 +1,95 @@
-import React, { Component, PropTypes } from 'react';
-import { ControlPool } from 'form-builder/components/ControlPool.jsx';
+import React, {Component, PropTypes} from 'react';
+import {ControlPool} from 'form-builder/components/ControlPool.jsx';
 import Canvas from 'form-builder/components/Canvas.jsx';
 import classNames from 'classnames';
 import ControlPropertiesContainer from 'form-builder/components/ControlPropertiesContainer.jsx';
-import { IDGenerator } from 'form-builder/helpers/idGenerator';
+import {IDGenerator} from 'form-builder/helpers/idGenerator';
 import FormHelper from 'form-builder/helpers/formHelper';
 
 export default class FormDetail extends Component {
-  constructor() {
-    super();
-    this.canvasRef = this.canvasRef.bind(this);
-    this.canvas = undefined;
-  }
-
-  getFormJson() {
-    if (this.canvas) {
-      return this.canvas.getWrappedInstance().prepareJson();
+    constructor() {
+        super();
+        this.canvasRef = this.canvasRef.bind(this);
+        this.canvas = undefined;
+        window.onscroll = function () {
+            if (window.scrollY <= 62) {
+                const element = document.getElementsByClassName("column-side");
+                element[0].className = "column-side"
+            }
+            if (window.scrollY >= 63 && window.scrollY <= 120) {
+                const element = document.getElementsByClassName("column-side");
+                element[0].className = "column-side scrolled"
+            }
+        }
     }
-    return null;
-  }
 
-  canvasRef(ref) {
-    this.canvas = ref;
-  }
+    getFormJson() {
+        if (this.canvas) {
+            return this.canvas.getWrappedInstance().prepareJson();
+        }
+        return null;
+    }
 
-  formTitle(name, version, published, editable) {
-    const status = published && !editable ? 'Published' : 'Draft';
-    const versionNumber = version ? `v${version}` : '';
-    return `${name} ${versionNumber} - ${status}`;
-  }
+    canvasRef(ref) {
+        this.canvas = ref;
+    }
 
-  render() {
-    const { formData } = this.props;
-    if (formData) {
-      const { name, uuid, id, version, published, editable } = this.props.formData;
-      const formResourceControls = FormHelper.getFormResourceControls(this.props.formData);
-      const idGenerator = new IDGenerator(formResourceControls);
-      return (
-        <div>
-          <div className="button-wrapper">
-          </div>
-          <div className={ classNames('container-main', { published: (published && !editable) }) }>
-            <h2 className="header-title">{this.formTitle(name, version, published, editable)}</h2>
-            <div className="container-columns">
-              <div className="column-side">
-                <ControlPool
-                  formResourceControls={formResourceControls}
-                  idGenerator={idGenerator}
-                />
-                <ControlPropertiesContainer />
-              </div>
-              <div className="container-column-main">
-                <div className="column-main">
-                  <Canvas
-                    formId={id}
-                    formName={name}
-                    formResourceControls={formResourceControls}
-                    formUuid={ uuid }
-                    idGenerator={idGenerator}
-                    ref={this.canvasRef}
-                  />
+    formTitle(name, version, published, editable) {
+        const status = published && !editable ? 'Published' : 'Draft';
+        const versionNumber = version ? `v${version}` : '';
+        return `${name} ${versionNumber} - ${status}`;
+    }
+
+    render() {
+        const {formData} = this.props;
+        if (formData) {
+            const {name, uuid, id, version, published, editable} = this.props.formData;
+            const formResourceControls = FormHelper.getFormResourceControls(this.props.formData);
+            const idGenerator = new IDGenerator(formResourceControls);
+            return (
+                <div>
+                    <div className="button-wrapper">
+                    </div>
+                    <div className={ classNames('container-main', {published: (published && !editable)}) }>
+                        <h2 className="header-title">{this.formTitle(name, version, published, editable)}</h2>
+                        <div className="container-columns">
+                            <div className="column-side">
+                                <ControlPool
+                                    formResourceControls={formResourceControls}
+                                    idGenerator={idGenerator}
+                                />
+                                <ControlPropertiesContainer />
+                            </div>
+                            <div className="container-column-main">
+                                <div className="column-main">
+                                    <Canvas
+                                        formId={id}
+                                        formName={name}
+                                        formResourceControls={formResourceControls}
+                                        formUuid={ uuid }
+                                        idGenerator={idGenerator}
+                                        ref={this.canvasRef}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      );
+            );
+        }
+        return null;
     }
-    return null;
-  }
 }
 
 FormDetail.propTypes = {
-  formData: PropTypes.shape({
-    id: PropTypes.number,
-    name: PropTypes.string.isRequired,
-    published: PropTypes.bool.isRequired,
-    resources: PropTypes.array,
-    uuid: PropTypes.string.isRequired,
-    version: PropTypes.string.isRequired,
-    editable: PropTypes.bool,
-  }),
-  setError: PropTypes.func.isRequired,
+    formData: PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string.isRequired,
+        published: PropTypes.bool.isRequired,
+        resources: PropTypes.array,
+        uuid: PropTypes.string.isRequired,
+        version: PropTypes.string.isRequired,
+        editable: PropTypes.bool,
+    }),
+    setError: PropTypes.func.isRequired,
 };
