d9723e3ac6885037f2d295f079e823b87ea19cf2
Venkata Jaswanth <aj.jaswanth@gmail.com>
2017-01-17 12:59:03 +0530

    Jaswanth | #2985 | Do not clear controlToConceptMap in state

--- src/form-builder/components/FormDetailContainer.jsx
+++ src/form-builder/components/FormDetailContainer.jsx
@@ -43,7 +43,6 @@ export class FormDetailContainer extends Component {
 
   componentWillUpdate() {
     this.props.dispatch(deselectControl());
-    this.props.dispatch(removeSourceMap());
     this.props.dispatch(removeControlProperties());
   }
 
