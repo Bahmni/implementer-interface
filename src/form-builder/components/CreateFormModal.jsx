5515dc3a69dd89f616bd7f6b2ec9334561c77fe6
xueying <xyli@thoughtworks.com>
2017-04-06 15:02:13 +0800

    Xueying | #3359 | can show error message when form name is more than 50

--- src/form-builder/components/CreateFormModal.jsx
+++ src/form-builder/components/CreateFormModal.jsx
@@ -1,61 +1,95 @@
-import React, { Component, PropTypes } from 'react';
+import React, {Component, PropTypes} from 'react';
+import classNames from 'classnames';
+import NotificationContainer from 'common/Notification';
+import { commonConstants } from 'common/constants';
 
 export default class CreateFormModal extends Component {
 
-  constructor() {
-    super();
-    this.createForm = this.createForm.bind(this);
-  }
-
-  setFormName(formName) {
-    this.formName = formName;
-  }
-
-  handleEsc(e) {
-    if (e.keyCode === 27) {
-      this.props.closeModal();
+    constructor() {
+        super();
+        this.createForm = this.createForm.bind(this);
+        this.state = {red: false, buttonDisable: false, notification: {}}
     }
-  }
 
-  createForm(e) {
-    e.preventDefault();
-    this.props.createForm(this.formName.trim());
-  }
-
-  render() {
-    if (this.props.showModal) {
-      return (
-        <div onKeyUp={(e) => this.handleEsc(e)}>
-          <div className="dialog-wrapper" onClick={this.props.closeModal}></div>
-          <div className="dialog">
-            <h2 className="header-title">Create a Form</h2>
-            <form className="dialog--container" onSubmit={(e) => this.createForm(e)}>
-              <div className="form-field clearfix">
-                <label>Form Name <span className="asterisk">*</span></label>
-                <input
-                  autoFocus
-                  className="form-name"
-                  onChange={(e) => this.setFormName(e.target.value)}
-                  type="text"
-                  maxLength="50"
-                />
-              </div>
-              <div className="button-wrapper fr">
-                <input className="button btn--highlight" type="submit" value="Create Form" />
-                <button className="btn" onClick={this.props.closeModal} type="reset">
-                  Cancel
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>);
+    setFormName(formName) {
+        this.formName = formName;
+        this.validateName(formName);
     }
-    return null;
-  }
+
+    handleEsc(e) {
+        if (e.keyCode === 27) {
+            this.props.closeModal();
+        }
+    }
+
+    createForm(e) {
+        e.preventDefault();
+        this.props.createForm(this.formName.trim());
+    }
+
+    validateName(value) {
+        if (value.length > 50) {
+            this.setState({red: true, buttonDisable: true})
+            this.setErrorMessage('Form name shall not exceed 50 characters');
+        } else {
+            this.setState({red: false, buttonDisable: false})
+        }
+    }
+
+    setErrorMessage(errorMessage) {
+        const errorNotification = {message: errorMessage, type: commonConstants.responseType.error};
+        this.setState({notification: errorNotification});
+        setTimeout(() => {
+            this.setState({notification: {}});
+        }, commonConstants.toastTimeout);
+    }
+
+    render() {
+        if (this.props.showModal) {
+            return (
+                <div>
+                    <NotificationContainer
+                        notification={this.state.notification}
+                    />
+                    <div onKeyUp={(e) => this.handleEsc(e)}>
+                        <div className="dialog-wrapper" onClick={this.props.closeModal}></div>
+                        <div className="dialog">
+                            <h2 className="header-title">Create a Form</h2>
+                            <form className="dialog--container" onSubmit={(e) => this.createForm(e)}>
+                                <div className="form-field clearfix">
+                                    <label>Form Name <span className="asterisk">*</span></label>
+                                    <input
+                                        autoFocus
+                                        className={ classNames('form-name', {'is-red': this.state.red})}
+                                        onChange={(e) => this.setFormName(e.target.value)}
+                                        pattern="[^\.\/\-\^\s][^\.\/\-\^]*"
+                                        required
+                                        title="Leading or trailing spaces and ^/-. are not allowed"
+                                        type="text"
+                                        onKeyDown={(e) => this.validateName(e.target.value)}
+                                    />
+                                </div>
+                                <div className="button-wrapper fr">
+                                    <input className="button btn--highlight"
+                                           type="submit"
+                                           value="Create Form"
+                                           disabled={this.state.buttonDisable}
+                                    />
+                                    <button className="btn" onClick={this.props.closeModal} type="reset">
+                                        Cancel
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>);
+        }
+        return null;
+    }
 }
 
 CreateFormModal.propTypes = {
-  closeModal: PropTypes.func.isRequired,
-  createForm: PropTypes.func.isRequired,
-  showModal: PropTypes.bool.isRequired,
+    closeModal: PropTypes.func.isRequired,
+    createForm: PropTypes.func.isRequired,
+    showModal: PropTypes.bool.isRequired,
 };
