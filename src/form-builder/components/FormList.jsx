7ab1e9fa04bf84a7b7f271cc482eb12e0855a9a1
xinzhang <xinzhang@thoughtworks.com>
2017-04-18 14:40:49 +0800

    Xueying & Xin | #3474 | Support to show message when download done.

--- src/form-builder/components/FormList.jsx
+++ src/form-builder/components/FormList.jsx
@@ -1,12 +1,44 @@
-import React, {Component, PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react';
 import _ from 'lodash';
-import {dateUtils} from 'common/utils/dateUtils';
-import {Link} from 'react-router';
-import {httpInterceptor} from "../../common/utils/httpInterceptor";
-import {formBuilderConstants} from "../constants";
+import { dateUtils } from 'common/utils/dateUtils';
+import { Link } from 'react-router';
+import { httpInterceptor } from '../../common/utils/httpInterceptor';
+import { formBuilderConstants } from '../constants';
 import fileDownload from 'react-file-download';
+import NotificationContainer from 'common/Notification';
+import { commonConstants } from '../../common/constants';
 
 export default class FormList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { notification: {} };
+  }
+
+  getRows() {
+    const data = _.map(this.props.data, (rowItem, index) => (
+      <tr key={rowItem.id}>
+        <td><i className=" fa fa-file-text-o" />{rowItem.name}</td>
+        <td>{rowItem.version}</td>
+        <td>{dateUtils.getDateWithoutTime(rowItem.auditInfo.dateCreated)}</td>
+        <td>{this._getFormStatus(rowItem)}</td>
+        <td className="edit-icon">{this._editOrReuseIcon(rowItem)}</td>
+        <td><a hidden={!rowItem.published}
+          onClick={() => this.downloadFile(index)}
+        >Export</a></td>
+      </tr>
+    ));
+    return data;
+  }
+
+  setMessage(messageText, type) {
+    const notification = { message: messageText, type };
+    this.setState({ notification });
+
+    setTimeout(() => {
+      this.setState({ notification: {} });
+    }, commonConstants.toastTimeout);
+  }
 
   downloadFile(index) {
     const form = this.props.data[index];
@@ -19,38 +51,23 @@ export default class FormList extends Component {
       .get(`${formBuilderConstants.formUrl}/${form.uuid}?${params}`)
       .then((data) => {
         fileDownload(JSON.stringify(data), `${fileName}.json`);
-
+        this.setMessage('Export Successfully', commonConstants.responseType.success);
       })
       .catch(() => {
-
-      })
-  }
-
-  getRows() {
-    const data = _.map(this.props.data, (rowItem, index) => (
-      <tr key={rowItem.id}>
-        <td><i className=" fa fa-file-text-o"/>{rowItem.name}</td>
-        <td>{rowItem.version}</td>
-        <td>{dateUtils.getDateWithoutTime(rowItem.auditInfo.dateCreated)}</td>
-        <td>{this._getFormStatus(rowItem)}</td>
-        <td className="edit-icon">{this._editOrReuseIcon(rowItem)}</td>
-        <td><a hidden={!rowItem.published}
-               onClick={(e) => this.downloadFile(index)}>Export</a></td>
-      </tr>
-    ));
-    return data;
+        this.setMessage('Export Failed', commonConstants.responseType.error);
+      });
   }
 
   _editOrReuseIcon(rowItem) {
     if (rowItem.published) {
       return (
-        <Link to={{pathname: `form-builder/${rowItem.uuid}`}}>
+        <Link to={{ pathname: `form-builder/${rowItem.uuid}` }}>
           <i className="fa fa-eye"></i>
         </Link>
       );
     }
     return (
-      <Link to={{pathname: `form-builder/${rowItem.uuid}`}}>
+      <Link to={{ pathname: `form-builder/${rowItem.uuid}` }}>
         <i className="fa fa-pencil"></i>
       </Link>
     );
@@ -66,19 +83,22 @@ export default class FormList extends Component {
 
   render() {
     return (this.props.data.length === 0 ? <p className="placeholder-text">No Forms to Display</p> :
+      <div>
+        <NotificationContainer notification={this.state.notification} />
         <table>
-          <thead>
-          <tr>
-            <th>Name</th>
-            <th>Version</th>
-            <th>Created On</th>
-            <th>Status</th>
-            <th>Action</th>
-            <th>...</th>
-          </tr>
-          </thead>
-          <tbody>{this.getRows()}</tbody>
-        </table>
+            <thead>
+            <tr>
+              <th>Name</th>
+              <th>Version</th>
+              <th>Created On</th>
+              <th>Status</th>
+              <th>Action</th>
+              <th>...</th>
+            </tr>
+            </thead>
+            <tbody>{this.getRows()}</tbody>
+          </table>
+      </div>
     );
   }
 }
--- test/form-builder/components/FormList.spec.js
+++ test/form-builder/components/FormList.spec.js
@@ -91,40 +91,38 @@ describe('FormList', () => {
   it('should render Export when form published', () => {
     wrapper = shallow(<FormList data={data} />);
 
-    expect(getItem(0,5).find('a').prop('hidden')).to.eql(true);
-    expect(getItem(1,5).find('a').prop('hidden')).to.eql(false);
-    expect(getItem(1,5).find('a').text()).to.eql('Export');
+    expect(getItem(0, 5).find('a').prop('hidden')).to.eql(true);
+    expect(getItem(1, 5).find('a').prop('hidden')).to.eql(false);
+    expect(getItem(1, 5).find('a').text()).to.eql('Export');
   });
 
   it('should call downloadFile when export be clicked', () => {
     wrapper = shallow(<FormList data={data} />);
     const spy = sinon.spy(wrapper.instance(), 'downloadFile');
 
-    const exportElement = getItem(1,5).find('a');
+    const exportElement = getItem(1, 5).find('a');
     exportElement.simulate('click');
 
     sinon.assert.calledOnce(spy);
   });
+
+  it('should render notification container', () => {
+    wrapper = shallow(<FormList data={data} />);
+
+    expect(wrapper.find('NotificationContainer')).to.have.length(1);
+  });
+
+  it('should call setMessage when download done', (done) => {
+    wrapper = shallow(<FormList data={data} />);
+    const spy = sinon.spy(wrapper.instance(), 'setMessage');
+
+    const exportElement = getItem(1, 5).find('a');
+    exportElement.simulate('click');
+
+    setTimeout(() => {
+      sinon.assert.calledOnce(spy);
+      done();
+    }, 500);
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
