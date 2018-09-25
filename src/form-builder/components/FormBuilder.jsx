4331c329128ac034d77b1e3ba5150b7cd4b93e4d
Sneha Bagri <snehabagri90@gmail.com>
2018-09-25 11:54:01 +0530

    Upgrade react to 15.6 and include proptypes package

--- src/form-builder/components/FormBuilder.jsx
+++ src/form-builder/components/FormBuilder.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import FormList from 'form-builder/components/FormList.jsx';
 import CreateFormModal from 'form-builder/components/CreateFormModal.jsx';
 import FormBuilderHeader from 'form-builder/components/FormBuilderHeader.jsx';
--- src/form-builder/components/FormDetail.jsx
+++ src/form-builder/components/FormDetail.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { ControlPool } from 'form-builder/components/ControlPool.jsx';
 import Canvas from 'form-builder/components/Canvas.jsx';
 import classNames from 'classnames';
