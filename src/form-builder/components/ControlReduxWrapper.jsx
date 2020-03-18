e28102bed6e57d4062f221ff0bad8d2f5c968deb
swetham <swetham@thoughtworks.com>
2020-03-18 14:51:58 +0530

    Sneha, Swetha | FBE-3 | fix eslint

--- src/form-builder/components/ControlReduxWrapper.jsx
+++ src/form-builder/components/ControlReduxWrapper.jsx
@@ -198,9 +198,9 @@ export class ControlWrapper extends Draggable {
     if (properties && properties.property &&
       (properties.id === this.metadata.id && properties.property.controlEvent)) {
       return (
-        <Popup className="form-event-popup"
+        <Popup className="form-event-popup" closeOnDocumentClick={false}
           open={properties.id === this.metadata.id && properties.property.controlEvent}
-          position="top center" closeOnDocumentClick={false}
+          position="top center"
         >
         <ScriptEditorModal
           close={() => this.closeScriptEditorDialog(properties.id)}
