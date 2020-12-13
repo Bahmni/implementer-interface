dce2705c931710fb7fe5c931a4973b0d4ec505bf
Buvaneswari Arun <buvaneswari.arun@thoughtworks.com>
2020-12-13 10:49:38 +0530

    BAH-1113|BAH790|Buvaneswari|Distortion in the Manage Privilege page - fix

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const routes = [
     exact: true,
     path: '/form-builder/:formUuid',
     title: 'Form Details',
-    siblingPath: '/form-builder',
+    siblingPath: '/form-builder/:formUuid/translate',
   },
   {
     component: FormTranslationsContainer,
@@ -43,7 +43,7 @@ const routes = [
     exact: true,
     path: '/form-builder/:formUuid/privilege',
     title: 'Show Form Privileges',
-    siblingPath: '/form-builder',
+    siblingPath: '/form-builder/:formUuid',
   },
 ];
 
