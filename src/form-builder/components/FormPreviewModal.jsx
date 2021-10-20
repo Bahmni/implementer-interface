0c492581bbb8c5bc84180682e247ebc85ba1d606
Himabindu T <tbindu@thoughtworks.com>
2021-10-20 16:38:26 +0530

    Bindu, Tarun | BAH-1017 | MOBN-1551 | Fix renderWithControls method in Preview tab (#7) (#61)
    * Bindu, Tarun | MOBN-1551 | Fix renderWithControls method in Preview tab
    
    * Bindu, Tarun | MOBN-1551 | Fix unit tests
    
    Co-authored-by: Himabindu Thungathurty <tbindu@thoughtworks.com>
    
    Co-authored-by: tarunkumar-tw <58736833+tarunkumar-tw@users.noreply.github.com>

--- src/form-builder/components/FormPreviewModal.jsx
+++ src/form-builder/components/FormPreviewModal.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import 'bahmni-form-controls/dist/helpers.js';
 import { Exception } from 'form-builder/helpers/Exception';
+import { Container } from 'bahmni-form-controls';
 
 
 export default class FormPreviewModal extends React.Component {
@@ -18,17 +20,17 @@ export default class FormPreviewModal extends React.Component {
     const defaultLocale = (window.localStorage
       && window.localStorage.getItem('openmrsDefaultLocale')) || 'en';
     this.state = {
-      formData: {},
-      container: {},
       defaultLocale,
+      recordTree: {},
     };
     this.onSave = this.onSave.bind(this);
-    this.setContainer = this.setContainer.bind(this);
+    this.updateContainer = this.updateContainer.bind(this);
+    this.onValueUpdated = this.onValueUpdated.bind(this);
   }
 
   componentDidMount() {
     try {
-      this.setContainer([]);
+      this.updateContainer([]);
     } catch (e) {
       this.props.setErrorMessage(new Exception(FormPreviewModal.formatErrors(e)).getException());
     }
@@ -36,52 +38,53 @@ export default class FormPreviewModal extends React.Component {
 
   onSave() {
     const formJson = this.props.formJson;
-    if (this.state.container.state && formJson.events.onFormSave) {
+    if (Object.keys(this.state.recordTree).length > 0 && formJson.events.onFormSave) {
       /* eslint-disable no-undef */
       try {
-        const records = runEventScript(this.state.container.state.data, formJson.events.onFormSave);
+        const records = runEventScript(this.state.recordTree, formJson.events.onFormSave);
         unMountForm(document.getElementById('form-container'));
-        this.setContainer(getObservations(records));
+        this.updateContainer(getObservations(records));
       } catch (e) {
         this.props.setErrorMessage(new Exception(FormPreviewModal.formatErrors(e)).getException());
       }
     }
   }
 
-  setContainer(observations) {
+  onValueUpdated(data) {
+    this.setState({ recordTree: data });
+  }
+
+  updateContainer(observations) {
     const metadata = this.props.formJson;
     if (metadata) {
-      const container = renderWithControls(metadata, observations, 'form-container', false, null,
-        false, this.state.defaultLocale, '');
-      this.setState({ container });
+      const container = React.createElement(Container,
+        { metadata, observations, validate: true, validateForm: false,
+          collapse: false, patient: null, locale: this.state.defaultLocale, translations: '',
+          onValueUpdated: this.onValueUpdated });
+      ReactDOM.render(container, document.getElementById('form-container'));
     }
   }
 
   render() {
     if (this.props.formJson) {
       return (
-                <div className="preview-container">
-                    <div className="preview-header">
-                        <div>Preview</div>
-                        <i className="fa fa-times" onClick={this.props.close}></i>
-                    </div>
-                    <div className="preview-body">
-                        <div className="disable-image-and-video-upload" id="form-container"> </div>
-                    </div>
-                    <div className="preview-footer">
-                        <button className="button btn--highlight" onClick={this.onSave}
-                          type="submit"
-                        >
-                          Save
-                        </button>
-                        <button className="btn preview-close-btn"
-                          onClick={this.props.close}
-                          type="reset"
-                        >
-                            Close
-                        </button>
-                    </div>
-                </div>);
+        <div className="preview-container">
+          <div className="preview-header">
+            <div>Preview</div>
+            <i className="fa fa-times" onClick={this.props.close}></i>
+          </div>
+          <div className="preview-body">
+            <div className="disable-image-and-video-upload" id="form-container"> </div>
+          </div>
+          <div className="preview-footer">
+            <button className="button btn--highlight" onClick={this.onSave} type="submit">
+              Save
+            </button>
+            <button className="btn preview-close-btn" onClick={this.props.close} type="reset">
+              Close
+            </button>
+          </div>
+        </div>);
     }
     return null;
   }
