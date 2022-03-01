531bd50d05194442030f2cd9c1f4883cb9590886
Abinaya U <abinaya.u@thoughtworks.com>
2022-03-01 17:47:03 +0530

    BAH-1410 |  refactor. use build and publish github action
    Co-authored-by: Umair Fayaz <omayrfayaz@gmail.com>

--- package/docker/scripts/docker_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -xe
-
-# yarn install --frozen-lockfile
-yarn ci
-
-#Building Docker images
-IMPLEMENTER_INTERFACE_IMAGE_TAG=${BAHMNI_VERSION:?}-${GITHUB_RUN_NUMBER:?}
-docker build -t bahmni/implementer-interface:${IMPLEMENTER_INTERFACE_IMAGE_TAG} -f package/docker/Dockerfile  . --no-cache
