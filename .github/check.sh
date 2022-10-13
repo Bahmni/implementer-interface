a15254fc8b46bae18bdd2844a74c37b9726ce815
stavtnameh <hemanth.v@beehyv.com>
2022-10-13 17:01:24 +0530

    Set environment variable to run another workflow
    Co-authored-by: Umair Fayaz <omayrfayaz@gmail.com>

--- .github/check.sh
+++ .github/check.sh
@@ -1,11 +1,23 @@
-#!/bin/bash
+!/bin/bash
+bahmni_image_data=$(echo $(curl -X GET https://hub.docker.com/v2/repositories/bahmni/implementer-interface/tags/latest) | jq -r '.tag_last_pushed' | cut -d 'T' -f 1)
+bahmni_image_creation_time =$(date -d $bahmni_image_data +%s)
+base_image_data=$(echo $(curl -X GET https://hub.docker.com/v2/repositories/library/httpd/tags/2.4-alpine) | jq -r '.tag_last_pushed' | cut -d 'T' -f 1)
+base_image_creation_time=$(date -d $base_image_data +%s)
 
-docker build -t bahmni/implementer-interface:local -f package/docker/Dockerfile .
-export LOCAL_SHA=$(docker inspect bahmni/implementer-interface:local | grep "Id" | cut -d ':' -f 3 | cut -d '"' -f 1)
-echo $LOCAL_SHA
-docker pull bahmni/implementer-interface:latest
-export REMOTE_SHA=$(docker inspect bahmni/implementer-interface:latest | grep "Id" | cut -d ':' -f 3 | cut -d '"' -f 1) 
-echo $REMOTE_SHA
-export IF_EQUALS=$([ "$LOCAL_SHA" = "$REMOTE_SHA" ] && echo "true" || echo "false")
-echo $IF_EQUALS
 
+# function check_if_needs_rebuild(org,image_name,image_tag,base_image_org,base_image_name,base_image_tag){
+bahmni_image_data=$(echo $(curl -X GET https://hub.docker.com/v2/repositories/bahmni/implementer-interface/tags/latest) | jq -r '.tag_last_pushed' | cut -d 'T' -f 1)
+bahmni_image_creation_time =$(date -d $bahmni_image_data +%s)
+base_image_data=$(echo $(curl -X GET https://hub.docker.com/v2/repositories/library/httpd/tags/2.4-alpine) | jq -r '.tag_last_pushed' | cut -d 'T' -f 1)
+base_image_creation_time=$(date -d $base_image_data +%s)
+
+if expr "$bahmni_image_creation_time" "<=" "$base_image_creation_time" >/dev/null; then
+    echo "bahmni image was created earlier than base image"
+    echo "$REBUILD=TRUE"
+else
+    echo "bahmni image was created later than base image"
+    echo "$REBUILD=FALSE"
+fi
+# }
+
+# check_if_needs_rebuild
