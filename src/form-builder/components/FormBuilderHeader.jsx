32f9ca9c9f2046aa2ee1fc57a46cdc6061ce7985
arshiyaTW2021 <arshiya.shehzad@thoughtworks.com>
2026-06-15 16:17:07 +0530

    BAH-4791. Fix lint errors

--- src/form-builder/components/FormBuilderHeader.jsx
+++ src/form-builder/components/FormBuilderHeader.jsx
@@ -8,25 +8,28 @@
  */
 
 import React from 'react';
-import {bahmniHomePath} from '../../common/constants';
+import { bahmniHomePath } from '../../common/constants';
 
 const Header = () => {
-  const homeUrl = (typeof localStorage !== 'undefined' && localStorage.getItem('homeUrl')) || bahmniHomePath;
-  return <div>
-    <div className="header-wrap">
-      <header className="header">
-        <nav className="nav">
-          <ul>
-            <li>
-              <a className="back-btn" href={homeUrl}>
-                <i className="fa fa-home"></i>
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+  const homeUrl = (typeof localStorage !== 'undefined' && localStorage.getItem('homeUrl'))
+    || bahmniHomePath;
+  return (
+    <div>
+      <div className="header-wrap">
+        <header className="header">
+          <nav className="nav">
+            <ul>
+              <li>
+                <a className="back-btn" href={homeUrl}>
+                  <i className="fa fa-home"></i>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Header;
